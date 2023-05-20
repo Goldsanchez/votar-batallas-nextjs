472306145698bef8f939119b7d04213992293cb7
Goldsanchez <goldsanchez@hotmail.com>
2023-05-20 17:01:02 -0500

    added signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { API_URL } from '../api/battles.api';
 
-export default function Login() {
+export default function Signup() {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -18,7 +18,7 @@ export default function Login() {
       password: password
     };
 
-    const { data } = await axios.post(`${API_URL}login/`, user, { headers: { 'Content-Type': 'application/json' } })   
+    const { data } = await axios.post(`${API_URL}signup/`, user, { headers: { 'Content-Type': 'application/json' } })   
     // localStorage.clear();
     localStorage.setItem('token', data.token);
     window.location.href = '/'
@@ -31,9 +31,10 @@ export default function Login() {
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
+              Sign Up
             </h2>
           </div>
+
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" method="POST" onSubmit={submit} >
               <div>
@@ -51,6 +52,7 @@ export default function Login() {
                   />
                 </div>
               </div>
+
               <div>
                 <div className="flex items-center justify-between">
                   <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
@@ -73,19 +75,21 @@ export default function Login() {
                   />
                 </div>
               </div>
+
               <div>
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                  Sign Up
                 </button>
               </div>
             </form>
+
             <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
+              Are you a member?{' '}
               <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Sign Up
+                Login
               </a>
             </p>
           </div>
@@ -94,3 +98,4 @@ export default function Login() {
     </div>
   )
 }
+
